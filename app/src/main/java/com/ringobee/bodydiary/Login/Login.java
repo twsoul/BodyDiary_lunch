8edab24e9241ee20f4586d9a293de2a307c6f563
tw_home <1013cm@naver.com>
2019-04-29 23:36:12 +0900

    비밀번호 설정 (진행중)

--- app/src/main/java/com/ringobee/bodydiary/Login/Login.java
+++ app/src/main/java/com/ringobee/bodydiary/Login/Login.java
@@ -23,30 +23,34 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
 
+        
         //처음에 비밀번호 저장을 했는지 판단
         SharedPreferences sf = getSharedPreferences("first_pw",MODE_PRIVATE);
         boolean first_pw_save =sf.getBoolean("first_pw",false);
+
         if(first_pw_save){ // 사용자가 처음에 비밀번호를 저장했다면
             super.onCreate(savedInstanceState);
             setContentView(R.layout.login_activity);
+
+            LoginInput = findViewById(R.id.input_pw);
+            AutoLogin = (CheckBox)findViewById(R.id.auto_login);
+            Login = (Button)findViewById(R.id.login);
+            FingerLogin = (Button)findViewById(R.id.finger_login);
+            ResetPw = findViewById(R.id.reset_pw);
+
+            LoginInput.setOnClickListener(this);
+            AutoLogin.setOnClickListener(this);
+            Login.setOnClickListener(this);
+            FingerLogin.setOnClickListener(this);
+            ResetPw.setOnClickListener(this);
         }else{ // 처음 비밀번호 설정 x
-            Intent i = new Intent(this, com.ringobee.bodydiary.Login.ResetPw.class);
+            Intent i = new Intent(this, ResetPw.class);
             startActivity(i);
         }
 
-
-        LoginInput = findViewById(R.id.input_pw);
-        AutoLogin = findViewById(R.id.auto_login);
-        Login = findViewById(R.id.login);
-        FingerLogin = findViewById(R.id.finger_login);
-        ResetPw = findViewById(R.id.reset_pw);
-
-//        LoginInput.setOnClickListener(this);
-        AutoLogin.setOnClickListener(this);
-        Login.setOnClickListener(this);
-        FingerLogin.setOnClickListener(this);
-        ResetPw.setOnClickListener(this);
 
     }
 
@@ -56,7 +60,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             case R.id.auto_login:
                 break;
             case R.id.login:
-                Intent i = new Intent();
+//                Intent i = new Intent();
                 break;
             case R.id.finger_login:
                 break;
